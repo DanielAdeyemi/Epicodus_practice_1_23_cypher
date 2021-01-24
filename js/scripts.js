5252be5bcd0e13020cc44f80e8448d36fa2acbcb
DanielAdeyemi <adeyemidany@gmail.com>
2021-01-24 10:55:08 -0800

    pass test 1: remove non-alphabetical characters from a string

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,14 @@
+function cypher(input) {
+  input = input.replace(/[^a-z]/g, '');
+  return input;
+}
+
 $(document).ready(function() {
   $('form').submit(function() {
     event.preventDefault();
-    alert('accepted');
+    let user = $('#input').val().toLowerCase();
+    user = cypher(user);
+    alert(user);
+
   });
 });
